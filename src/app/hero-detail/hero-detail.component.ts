cbe2a7f6a94e40294eaaebca43780623a80ae22f
Ryan Ballenger <ryan@Ryans-MacBook-Pro.local>
2019-03-24 18:09:05 -0400

    finished

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
 
+import { ActivatedRoute } from '@angular/router'; 
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 import { Component, OnInit, Input } from '@angular/core';
 
@@ -13,9 +16,26 @@ export class HeroDetailComponent implements OnInit {
 
 @Input() hero: Hero;
 
-  constructor() { }
+  constructor(
+	private route: ActivatedRoute, 
+	private heroService: HeroService, 
+	private location: Location
+  ) { }
 
-  ngOnInit() {
-  }
+	ngOnInit(): void { this.getHero();}
+
+	getHero(): void {
+		const id = +this.route.snapshot.paramMap.get('id'); this.heroService.getHero(id)
+.subscribe(hero => this.hero = hero); }
+
+
+	goBack(): void {
+  		this.location.back();
+	}
+
+	save(): void {
+   		this.heroService.updateHero(this.hero)
+     		.subscribe(() => this.goBack());
+ 	}
 
 }
---
+++ src/app/message.service.ts
@@ -0,0 +1,29 @@
+
+
+
+import { Injectable } from '@angular/core';
+
+
+
+@Injectable({ providedIn: 'root',
+})
+export class MessageService {
+
+	messages: string[] = [];
+	
+	add(message: string) { this.messages.push(message);}
+
+
+	clear() { this.messages = [];} 
+
+
+}
+
+
+
+
+
+
+
+
+
